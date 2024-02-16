5a42d5156c01b828b59566c02b063490888c2096
PranayManchanda <pranaykuwait@gmail.com>
2024-02-15 19:13:51 -0500

    updated ImageLinkForm.js

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://arcane-sea-48936-cbc80ecb9a73.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -75,7 +75,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://arcane-sea-48936-cbc80ecb9a73.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
